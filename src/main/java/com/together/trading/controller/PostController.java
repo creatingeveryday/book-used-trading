f8abd821d24df3ee0aaa5d73fb8ad27608c9ad07
JongKuk Seo <jongkuk.dev@gmail.com>
2023-03-16 00:12:15 +0900

    feat: post controller 추가

---
+++ src/main/java/com/together/trading/controller/PostController.java
@@ -0,0 +1,31 @@
+package com.together.trading.controller;
+
+import com.together.trading.post.domain.dto.PostDto;
+import com.together.trading.post.domain.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/post")
+@RestController
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping("/save")
+    public Long save(@RequestBody PostDto postDto) {
+        return postService.save(postDto);
+    }
+
+    @GetMapping("/{id}")
+    public PostDto showPostById(@PathVariable Long id) {
+        return postService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostDto postDto) {
+        return postService.update(id, postDto);
+    }
+
+
+}
